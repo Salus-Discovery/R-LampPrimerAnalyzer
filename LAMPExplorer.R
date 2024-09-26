d5d1fe2dfc5c15963df6821d75293000d6fe472b
SalusDiscovery <57226743+SalusDiscovery@users.noreply.github.com>
2024-09-26 10:49:17 -0500

    Add comment for XNAString install

--- LAMPExplorer.R
+++ LAMPExplorer.R
@@ -16,7 +16,10 @@
 # new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 # if(length(new.packages)) install.packages(new.packages)
 
-
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("XNAString")
 
 
 library(shiny)
